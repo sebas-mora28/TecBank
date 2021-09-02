fbb147a69865f19bb9b915379b99538fa241dcf1
Shakime <jeykimers.johnson@gmail.com>
2021-09-01 19:42:09 -0600

    [UPDATE] Se agrega ID al pago y se corrige el mes en la fecha.

--- API/Models/Pago.cs
+++ API/Models/Pago.cs
@@ -2,7 +2,8 @@
 namespace API.Models
 {
     public class Pago
-    {
+    {   
+        public long Id {get;set;}
         public long Monto{ get; set; }
         public string Fecha{ get; set; }
     }
